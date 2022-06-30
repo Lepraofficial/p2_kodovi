7a8bc91d549a3eedb030c10072b9b62a10adbadc
Lepraofficial <106381371+Lepraofficial@users.noreply.github.com>
2022-06-30 23:14:47 +0200

    skup zadatak
    radi sa intovima i koristi dinamicki alociran niz

--- skup.cpp
+++ skup.cpp
@@ -1,134 +1,133 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+
 using namespace std;
 
-
-template <typename T>
 class Skup {
-    private:
-        vector<T> s;
+     int* niz;
+     int kapacitet, popunjeno;
+
     public:
-        Skup() {}
-        ~Skup() { s.clear(); }
-        int size() { return s.size(); }
-        void push_back(T x) { s.push_back(x); }
 
-        void ucitajElemente(T arr[], size_t arrSize) {
-            for (int i = 0; i < arrSize; i++) {
-                s.push_back(arr[i]);
+    Skup(int kap = 10):kapacitet(kap), popunjeno(0){
+        niz = new int[kapacitet];
+    }
+
+    ~Skup(){delete[]niz;};
+    Skup(const Skup& org){
+        kapacitet= org.kapacitet;
+        popunjeno = org.popunjeno;
+
+        niz = new int[kapacitet];
+
+        for(int i(0);i<popunjeno;i++){
+           niz[i] = org.niz[i];
+        }
+
+    };
+
+    void operator=(const Skup &org){
+        kapacitet= org.kapacitet;
+        popunjeno = org.popunjeno;
+        delete [] niz;
+        niz = new int[kapacitet];
+
+        for(int i(0);i<popunjeno;i++){
+           niz[i] = org.niz[i];
+        }
+    };
+
+    void dodaj(int novi){
+        if(popunjeno >= kapacitet){
+            cout<<"popunjen skup"<<endl;
+        }
+        else{
+            niz[popunjeno] = novi;
+            popunjeno++;
+        }
+    };
+
+    friend bool imal(Skup s, int el){
+        for(int i(0); i<s.popunjeno;i++){
+            if(s.niz[i] == el) return true;
+        }
+        return false;
+    }
+
+    void dodajAkoNema(int el){
+        for( int i(0);i<popunjeno+1;i++){
+            if(el == niz[i] && !(popunjeno == 0)) break;
+            if(i == popunjeno) {dodaj(el); break;};
+        }
+    };
+
+    void ispisi(){
+        cout<<"{"<<niz[0]<<", ";
+        for(int i(1); i<popunjeno;i++){
+            cout<<niz[i]<<", ";
+        }
+        cout<<"}";
+    };
+
+    friend void presjek(Skup s1, Skup s2){
+        Skup novi(s1.popunjeno + s2.popunjeno + 5);
+
+        for(int i(0);i<s1.popunjeno;i++){
+            for(int j(0);j<s2.popunjeno;j++){
+                if(s1.niz[i] == s2.niz[j]) novi.dodajAkoNema(s1.niz[i]);
             }
         }
 
-        void prikazi() {
-            for (int i = 0; i < s.size(); i++) {
-                cout << s[i] << " "; 
+        novi.ispisi();
+    };
+
+    friend void unija(Skup s1, Skup s2) {
+        Skup novi(s1.popunjeno + s2.popunjeno + 5);
+
+        for( int i(0); i<s1.popunjeno;i++){novi.dodajAkoNema(s1.niz[i]);}
+        for( int i(0); i<s2.popunjeno;i++){novi.dodajAkoNema(s2.niz[i]);}
+
+        novi.ispisi();
+    }
+
+    friend void razlika(Skup s1, Skup s2){
+    Skup novi(s1.popunjeno + s2.popunjeno + 5);
+
+        for(int i(0);i<s1.popunjeno;i++){
+            for(int j(0);j<s2.popunjeno;j++){
+                if(s1.niz[i] == s2.niz[j]) novi.dodajAkoNema(s1.niz[i]);
             }
         }
 
-        void unija(Skup<T> s1, Skup<T> s2) {
-            Skup <T> u;
-            vector<bool> v;
+    Skup razlikaa(s1.popunjeno + s2.popunjeno + 5);
 
-            // vektor koji odredjuje koje cemo uzet elemente iz s2
-            for (int i = 0; i < s2.size(); i++) {
-                v.push_back(false);
-            }
-
-            for (int i = 0; i < s1.size(); i++) {
-                u.push_back(s1.s[i]);
-            }
-            
-            // flaggamo elemente iz s2
-            for (int i = 0; i < s2.size(); i++) {
-                for (int j = 0; j < s1.size(); j++) {
-                    if (s2.s[i] == u.s[j]) {
-                        v[i] = true;
-                    } 
-                }
-            }
-
-            for (int i = 0; i < v.size(); i++) {
-                if (v[i] == false) {
-                    u.push_back(s2.s[i]);
-                }
-            }
-            u.prikazi();
+    for(int i(0);i<s1.popunjeno;i++){
+            if(!imal(novi, s1.niz[i])) razlikaa.dodajAkoNema(s1.niz[i]);
         }
-
-        void presjek(Skup <T> s1, Skup <T> s2) {
-            Skup <T> p;
-            vector<T> copy;
-
-            // skupimo sve elemente u jedan skup, sortiramo i trazimo dva ista uzastopna elementa
-            for (int i = 0; i < s1.size(); i++) {
-                copy.push_back(s1.s[i]);
-            }
-            for (int i = 0; i < s2.size(); i++) {
-                copy.push_back(s2.s[i]);
-            }
-            sort(copy.begin(), copy.end());
-
-            for (int i = 0; i < copy.size(); i++) {
-                if (copy[i] == copy[i+1]) {
-                    p.push_back(copy[i]);
-                }
-            }
-            p.prikazi();
+    for(int i(0);i<s2.popunjeno;i++){
+            if(!imal(novi, s2.niz[i])) razlikaa.dodajAkoNema(s2.niz[i]);
         }
-
-        void razlika(Skup <T> s1, Skup <T> s2) {
-            Skup <T> r;
-            vector<T> copy;
-
-            for (int i = 0; i < s1.size(); i++) {
-                copy.push_back(s1.s[i]);
-            }
-            for (int i = 0; i < s2.size(); i++) {
-                copy.push_back(s2.s[i]);
-            }
-            sort(copy.begin(), copy.end());
-
-            for (int i = 0; i < copy.size(); i++) {
-                if (copy[i] != copy[i+1] && copy[i] != copy[i-1]) {
-                    r.push_back(copy[i]);
-                }
-            }
-            r.prikazi();
-        }
-};      
+    razlikaa.ispisi();
+    };
 
 
-int main() {
-    Skup <int> s1;
-    Skup <int> s2;
-    Skup <int> s3;
-    int arr[] = {1, 2, 3, 4};
-    int arr2[] = {4, 5, 6};
+};
 
-    size_t arr1Size = sizeof(arr) / sizeof(*arr);
-    size_t arr2Size = sizeof(arr2) / sizeof(*arr2);
-    
-    s1.ucitajElemente(arr, arr1Size);
-    s2.ucitajElemente(arr2, arr2Size);
+int main(){
+    Skup prvi, drugi(5);
 
-    // Ispod je samo kod za ispisivanje
-    // =====================================
-    cout << "Skup 1: ";
-    s1.prikazi();
-    cout << endl;
-    cout << "Skup 2: ";
-    s2.prikazi();
-    cout << endl;
-    cout << "Unija skupova s1 i s2: ";
-    s3.unija(s1, s2);
-    cout << endl;
-    cout << "Presjek skupova s1 i s2: ";
-    s3.presjek(s1, s2);
-    cout << endl;
-    cout << "Razlika skupova s1 i s2: ";
-    s3.razlika(s1, s2);
-    // =====================================
+    for(int i(0);i<10;i++){prvi.dodaj(i);}
+    prvi.ispisi(); cout<<endl;
+    for(int i(4);i<8;i++){drugi.dodaj(i);}
+    drugi.dodaj(12);
+    drugi.dodaj(14);
 
-    return 0;
+    drugi.ispisi(); cout<<endl;
+    cout<<endl<<"------------------"<<endl<<"presjek: ";
+    presjek(prvi, drugi);
+    cout<<endl<<"------------------"<<endl<<"unija: ";
+    unija(prvi, drugi);
+
+    cout<<endl<<"------------------"<<endl<<"razlika: ";
+    razlika(prvi, drugi);
+
 }
